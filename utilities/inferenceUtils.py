d2effbf1a2bd2aeb0385e64567c6fe65383a060f
jaewon2753-droid <jaewon2753@gmail.com>
2025-08-30 03:25:33 +0900

    Update inferenceUtils.py

--- utilities/inferenceUtils.py
+++ utilities/inferenceUtils.py
@@ -1,9 +1,11 @@
-import torch 
+# utilities/inferenceUtils.py
+
+import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
-import os 
+import os
 import glob
 from shutil import copyfile
 import matplotlib.pyplot as plt
@@ -14,6 +16,10 @@ import cv2
 from PIL import Image
 from dataTools.dataNormalization import *
 from PIL import ImageFile
+# ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ 불량 화소 생성기 import ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+from dataTools.badPixelGenerator import generate_bad_pixels
+# ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class AddGaussianNoise(object):
@@ -21,18 +27,20 @@ class AddGaussianNoise(object):
         self.var = 0.1
         self.mean = 0.0
         self.noiseLevel = noiseLevel
-        
+
     def __call__(self, tensor):
         sigma = self.noiseLevel/100.
         noisyTensor = tensor + torch.randn(tensor.size()).uniform_(0, 1.) * sigma  + self.mean
-        return noisyTensor 
-    
+        return noisyTensor
+
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.var)
 
 
 class inference():
-    def __init__(self, gridSize, inputRootDir, outputRootDir, modelName, resize = None, validation = None ):
+    # ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ defectiveDir 파라미터 추가 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+    def __init__(self, gridSize, inputRootDir, outputRootDir, modelName, resize = None, validation = None, defectiveDir = "./defective_output/"):
+    # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
         self.inputRootDir = inputRootDir
         self.outputRootDir = outputRootDir
         self.gridSize = gridSize
@@ -40,56 +48,48 @@ class inference():
         self.resize = resize
         self.validation = validation
         self.unNormalize = UnNormalize()
-    
+        # ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ 불량화소 이미지 저장 경로 설정 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+        self.defectiveDir = defectiveDir
+        createDir(self.defectiveDir)
+        # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
 
     def inputForInference(self, imagePath, noiseLevel):
         img = Image.open(imagePath)
-        #print(imagePath, img.size)
-        #print (img.size)
-        '''if  img.size[0]>1024:
-            print("Image Resized",imagePath, img.size)
-            resizeDimension =  (1024, img.size[1]) 
-            img = img.resize(resizeDimension)
-            print ("New Image Dimesion:", img.size)
-            img.save(imagePath) 
-
-        if  img.size[1]>1024:
-            print("Image Resized",imagePath, img.size)
-            resizeDimension =  (img.size[0], 1024) 
-            img = img.resize(resizeDimension)
-            print ("New Image Dimesion:", img.size)
-            img.save(imagePath)''' 
-
-        '''if  img.size[0]<600 or img.size[1]<600:
-            #print("Image Resized",imagePath, img.size)
-            resizeDimension =  (512, 512) 
-            img = img.resize(resizeDimension)
-            #print ("New Image Dimesion:", img.size)
-            img.save(imagePath) '''
 
         if  ("McM" in imagePath) or ("WED" in imagePath) or ("BSD" in imagePath):
-            #print("Image Resized",imagePath, img.size)
-            resizeDimension =  (512, 512) 
+            resizeDimension =  (512, 512)
             img = img.resize(resizeDimension)
-            img.save(imagePath) 
+            img.save(imagePath)
         if "Urban" in imagePath:
-            #print("Image Resized",imagePath, img.size)
-            resizeDimension =  (1024, 1024) 
+            resizeDimension =  (1024, 1024)
             img = img.resize(resizeDimension)
-            img.save(imagePath)  
+            img.save(imagePath)
 
-        img = np.asarray(img) 
-        if self.gridSize == 1 : 
-            img = bayerSampler(img)
-        elif self.gridSize == 2 : 
-            img = quadBayerSampler(img)
-        elif self.gridSize == 3 : 
-            img = dynamicBayerSampler(img, gridSze)
-        img = Image.fromarray(img)
+        img_np = np.asarray(img)
+        if self.gridSize == 1 :
+            img_np = bayerSampler(img_np)
+        elif self.gridSize == 2 :
+            img_np = quadBayerSampler(img_np)
+        elif self.gridSize == 3 :
+            img_np = dynamicBayerSampler(img_np, gridSze)
+        
+        # ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ 불량 화소 생성 및 저장 로직 추가 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+        # 1. 불량 화소 생성
+        defective_img_np = generate_bad_pixels(img_np)
+
+        # 2. 불량 화소가 적용된 이미지 저장
+        datasetName = imagePath.split("/")[-2]
+        defective_save_dir = os.path.join(self.defectiveDir, self.modelName, datasetName)
+        createDir(defective_save_dir)
+        defective_image_path = os.path.join(defective_save_dir, extractFileName(imagePath, True) + "_defective.png")
+        Image.fromarray(defective_img_np).save(defective_image_path)
+
+        # 3. PIL 이미지로 변환하여 이후 프로세스 진행
+        img = Image.fromarray(defective_img_np)
+        # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
         if self.resize:
-            #resize(256,256)
             transform = transforms.Compose([ transforms.Resize(self.resize, interpolation=Image.BICUBIC) ])
             img = transform(img)
 
@@ -99,37 +99,31 @@ class inference():
 
         testImg = transform(img).unsqueeze(0)
 
-        return testImg 
+        return testImg
 
 
     def saveModelOutput(self, modelOutput, inputImagePath, noiseLevel, step = None, ext = ".png"):
         datasetName = inputImagePath.split("/")[-2]
         if step:
             imageSavingPath = self.outputRootDir + self.modelName  + "/"  + datasetName + "/" + extractFileName(inputImagePath, True)  + \
-                            "_sigma_" + str(noiseLevel) + "_" + self.modelName + "_" + str(step) + ext
+                            "_sigma_" + str(noiseLevel) + "_" + self.modelName + "_" + str(step) + "_restored" + ext
         else:
             imageSavingPath = self.outputRootDir + self.modelName  + "/"  + datasetName + "/" + extractFileName(inputImagePath, True)  + \
-                            "_sigma_" + str(noiseLevel) + "_" + self.modelName + ext
-        
+                            "_sigma_" + str(noiseLevel) + "_" + self.modelName + "_restored" + ext
+
         save_image(self.unNormalize(modelOutput[0]), imageSavingPath)
 
-    
+
 
     def testingSetProcessor(self):
 
         testSets = glob.glob(self.inputRootDir+"*/")
-        #print (testSets)
         if self.validation:
-            #print(self.validation)
             testSets = testSets[:1]
-        #print (testSets)
         testImageList = []
         for t in testSets:
-            #print (t.split("/")[-2])
             testSetName = t.split("/")[-2]
             createDir(self.outputRootDir + self.modelName  + "/" + testSetName)
             imgInTargetDir = imageList(t, False)
             testImageList += imgInTargetDir
         return testImageList
-
-
